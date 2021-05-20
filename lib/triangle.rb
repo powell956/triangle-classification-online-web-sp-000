18bbde1119a26e01a3fca4cc7acdca990d7698d7
Dan Powell <powell956@gmail.com>
2021-05-20 15:16:44 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+
+    if a == b && b == c 
+      :equilateral
+    elsif a == b || a == c || b == c 
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle 
+    valid = [(a + b > c), (a + c > b), (b + c > a)]
+
+    [a, b, c].each do |side|
+      valid << false if side <= 0
+    end
+
+    if valid.include?(false)
+      raise TriangleError
+    end
+  end
+  
+  
+  class TriangleError < StandardError
+    def message
+      "One side of your triangle is too long!"
+    end
+  end
 end
